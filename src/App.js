e9369d0b3c5e41b37dc5e1a0256c1fab9d16a268
Tomasz <80126476+TOMEOv17@users.noreply.github.com>
2023-08-03 17:44:02 +0200

    initial styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import './styles/app.css'
 function App() {
   return (
     <div className="app">
-      eee
+
+      <div className="current_weather">
+        <input type="text" placeholder="Enter location..."/>
+        <div className="overall">
+          <p>New York</p>
+          <h1>34°C</h1>
+          <p>Clouds</p>
+        </div>
+        <div className="details">
+          <div className="feels_like">
+            <b>33.6°F</b>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <b>70%</b>
+            <p>Humidity</p>
+          </div>
+          <div className="wind_speed">
+            <b>1.99 MPH</b>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="forecast"></div>
+
     </div>
   );
 }
